804f271a84f9210793dbab5a4c09ba90f50e86eb
silveiraluisf <silveiraluis88@gmail.com>
2022-10-06 14:06:57 -0300

    feito primeira vesão dos cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,41 +1,145 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen ({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Galeria de arte pessoal'),
+        title: const Text('Galeria de arte pessoal'),
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(child: Text('Galeria', style: TextStyle(color: Colors.white, fontSize: 36),)
-            ),
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-              ),
-              elevation: 6,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 6,
+            child: SizedBox(
+              width: double.infinity,
               child: Column(
                 children: <Widget>[
                   Stack(
                     children: <Widget>[
                       ClipRRect(
                         borderRadius: BorderRadius.circular(10),
-                        child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Ct765BUL-eQPc-JhOva8GXcjyOJMbck-Qw&usqp=CAU'),
+                        child: Image.network(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Ct765BUL-eQPc-JhOva8GXcjyOJMbck-Qw&usqp=CAU',
+                          width: double.infinity,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      
+                      Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black54,
+                        padding: const EdgeInsets.all(6),
+                        child: const Positioned(
+                          bottom: 20,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'Musicas',
+                              style: TextStyle(
+                                  color: Colors.white70, fontSize: 24),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   )
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+          Card(
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 6,
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          'https://mymodernmet.com/wp/wp-content/uploads/2018/05/painting-ideas-thumbnail-jpg.jpg',
+                          width: double.infinity,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black54,
+                        padding: const EdgeInsets.all(6),
+                        child: const Positioned(
+                          bottom: 20,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'Pinturas',
+                              style: TextStyle(
+                                  color: Colors.white70, fontSize: 24),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Card(
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 6,
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          'https://www.sciencealert.com/images/2021-06/processed/PersonWritingInJournalWithBlueInkPen_600.jpg',
+                          width: double.infinity,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black54,
+                        padding: const EdgeInsets.all(6),
+                        child: const Positioned(
+                          bottom: 20,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'Textos',
+                              style: TextStyle(
+                                  color: Colors.white70, fontSize: 24),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
